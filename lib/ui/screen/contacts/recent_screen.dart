99a499088c2aaccb0a1f48155c05adc3698a7673
aek <konan.kouame@soprasteria.com>
2021-05-17 23:51:59 +0200

    common appbar

--- lib/ui/screen/contacts/recent_screen.dart
+++ lib/ui/screen/contacts/recent_screen.dart
@@ -8,8 +8,59 @@ class RecentScreen extends StatefulWidget {
 class _RecentScreenState extends State<RecentScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        child: Center(
+    return  Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          flexibleSpace: SafeArea(
+              bottom: false,
+              child: Column(children: <Widget>[
+                SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Container(
+                              height: 45,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.grey[100]),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(
+                                      Icons.search,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                    suffixIcon: IconButton(
+                                        icon: Icon(
+                                          Icons.qr_code,
+                                          color: Theme.of(context).primaryColor,
+                                        ),
+                                        onPressed: () =>{})),
+                                style: TextStyle(
+                                    color: Colors.black87.withOpacity(0.6)),
+                              ),
+                            ),
+                          )),
+                      Expanded(
+                        flex: 0,
+                        child: Icon(
+                          Icons.tune_outlined,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ])),
+        ),
+        body: Center(
         child: Text('recent'),
     )
     );
