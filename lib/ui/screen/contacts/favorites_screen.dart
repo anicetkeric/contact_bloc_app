85c5530d65dce11fbc59c3e1722498187866b9d1
aek <konan.kouame@soprasteria.com>
2021-06-01 21:21:48 +0200

    favorites list view

--- lib/ui/screen/contacts/favorites_screen.dart
+++ lib/ui/screen/contacts/favorites_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
+import 'package:contact_bloc_app/data/json/dummy_json.dart';
 import 'package:contact_bloc_app/ui/widget/app_bar.dart';
-import 'package:contact_bloc_app/ui/widget/dialpad_floating_action.dart';
+import 'package:contact_bloc_app/ui/widget/new_contacts_floating_action.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteScreen extends StatefulWidget {
@@ -8,15 +12,60 @@ class FavoriteScreen extends StatefulWidget {
 }
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
+
+  bool isLandscape(BuildContext context) =>
+      MediaQuery.of(context).orientation == Orientation.landscape;
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
         appBar: Appbar(),
-        floatingActionButton: DialPadFloatingAction(),
+        floatingActionButton: NewContactFloatingAction(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        body: Center(
-          child: Text('recent'),
-        )
+        body: _favoritesCard()
+    );
+  }
+
+  Widget _favoritesCard() {
+    return SafeArea(
+      child: GridView.count(
+        crossAxisCount: 2,
+        shrinkWrap: true,
+        physics: ScrollPhysics(),
+        padding: EdgeInsets.all(5),
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        childAspectRatio: 1.2,
+        children: List.generate(contactDummyData.length, (index) {
+          return Container(
+            padding: EdgeInsets.fromLTRB(10,10,10,0),
+            height: 100,
+            width: double.maxFinite,
+            child: Card(
+              elevation: 5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                    child: Text(
+                      contactDummyData[index].name.substring(0, 1).toUpperCase(),
+                      style: TextStyle(fontSize: 18, color: Colors.white70),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    padding: new EdgeInsets.only(right: 10.0, left: 10.0),
+                    child: Text(contactDummyData[index].name,
+                      overflow: TextOverflow.ellipsis,),
+                  )
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
     );
   }
 }
--- lib/ui/widget/new_contacts_floating_action.dart
+++ lib/ui/widget/new_contacts_floating_action.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DialPadFloatingAction extends StatelessWidget {
+class NewContactFloatingAction extends StatelessWidget {
 
 
   @override
